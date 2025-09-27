5e502a1bb7bc6bf9ae6767458c08dec75d3681d3
iampratik13 <bhagwatpratik781@gmail.com>
2025-09-27 12:20:22 +0530

    Add initial backend setup

---
+++ backend/dist/Game.js
@@ -0,0 +1,71 @@
+import { Chess } from "chess.js";
+import { WebSocket } from "ws";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages.js";
+export class Game {
+    player1;
+    player2;
+    board;
+    startTime;
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new Chess();
+        this.startTime = new Date();
+        this.player1.send(JSON.stringify({
+            type: INIT_GAME,
+            payload: {
+                color: 'white',
+            }
+        }));
+        this.player2.send(JSON.stringify({
+            type: INIT_GAME,
+            payload: {
+                color: 'black',
+            }
+        }));
+    }
+    makeMove(socket, move) {
+        if (this.board.history().length === 0 && socket !== this.player1) {
+            return;
+        }
+        if (this.board.history().length % 2 === 1 && socket !== this.player2) {
+            return;
+        }
+        try {
+            this.board.move(move);
+        }
+        catch (e) {
+            console.log(e);
+            return;
+        }
+        if (this.board.isGameOver()) {
+            this.player1.send(JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winnner: this.board.turn() === 'w' ? 'black' : 'white'
+                }
+            }));
+            this.player2.send(JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winnner: this.board.turn() === 'w' ? 'black' : 'white'
+                }
+            }));
+            return;
+        }
+        // Send the move to the opponent
+        if (socket === this.player1) {
+            this.player2.send(JSON.stringify({
+                type: MOVE,
+                payload: move
+            }));
+        }
+        else {
+            this.player1.send(JSON.stringify({
+                type: MOVE,
+                payload: move
+            }));
+        }
+    }
+}
+//# sourceMappingURL=Game.js.map
